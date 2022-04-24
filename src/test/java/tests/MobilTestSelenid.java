c1153417d46a8d37d962e93087cae82bd4c37616
Alexandr Donskov <mrad@list.ru>
2022-04-24 19:06:42 +0300

    update test

--- src/test/java/tests/MobilTestSelenid.java
+++ src/test/java/tests/MobilTestSelenid.java
@@ -13,16 +13,16 @@ import static io.qameta.allure.Allure.step;
 public class MobilTestSelenid extends TestBase{
     @Test
     void newSearchTest() {
-        step("Skip onboarding", Selenide::back);
-//        step("Skip Language Selection", () ->
-//                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click());
+//        step("Skip onboarding", Selenide::back);
+        step("Skip Language Selection", () ->
+                $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click());
 
         step("Type search", () -> {
             $(MobileBy.AccessibilityId("Search Wikipedia")).click();
             $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("Moscow");
         });
 
-        step("Skip onboarding", Selenide::back);
+//        step("Skip onboarding", Selenide::back);
 
         step("Verify content found", () ->
                 $$(byClassName("android.widget.TextView")).shouldHave(sizeGreaterThan(0)));
